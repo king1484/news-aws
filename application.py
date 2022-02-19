e74359e3812d9daab980fe16dc355f4cc3d2adb9
king1484 <85832194+king1484@users.noreply.github.com>
2022-02-19 19:08:10 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,jsonify
 from newspaper import Article
 from deep_translator import GoogleTranslator
+from newspaper import Config
 
 application = Flask(__name__)
 
@@ -12,13 +13,19 @@ def data():
 def get():
     url = request.form.get("url")
     lang = request.form.get("lang")
-    article = Article(url)
+    
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0'
+    config = Config()
+    config.browser_user_agent = user_agent
+    config.request_timeout = 10
+    article = Article(url,config=config)
+    
     article.download()
     article.parse()
     title=""
     text=""
     if lang!="en":
-        translated = GoogleTranslator(target=lang).translate_batch([article.title, article.text])
+        translated = GoogleTranslator(target=lang).translate_batch([article.title, (article.text[:4900]+"...") if len(article.text)>5000 else article.text])
         title = translated[0]
         text = translated[1]
     else:
